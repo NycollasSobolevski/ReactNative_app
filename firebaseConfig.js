58cfc2aaa4e9a8597683c696ed974dec1435d720
Nycollas sobolevski <sobolevski.academy@gmail.com>
2023-06-27 16:14:49 -0300

    UPDATE: to sync

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB6w0M07CcSNWw8O-BzfDN8NQTDNArCiZ0",
   authDomain: "react-app-3464c.firebaseapp.com",
